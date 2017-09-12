c7cc4c0e3d32de8370488c13227dc3df4d6430fb
gazizi <ghodrat.azizi@gmail.com>
2017-09-12 11:27:20 -0400

    new url14

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import urllib.request
 import base64
 import json
 import os
-from http import HTTPStatus
+
 from flask import Flask
 from flask import request
 from flask import make_response
@@ -49,7 +49,7 @@ def makeURLResult(req):
     rsp = urllib.request.urlopen(request)
     rsp_response_code = rsp.getcode()
 
-    if rsp_response_code is HTTPStatus.OK
+    if rsp_response_code is 200
         json_data = json.load(rsp)
     else
         data = {}
