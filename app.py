82f424a0b5161f73c6f0a8e475e63267b4b9cde4
gazizi <ghodrat.azizi@gmail.com>
2017-09-12 11:25:57 -0400

    new url13

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def makeURLResult(req):
     rsp = urllib.request.urlopen(request)
     rsp_response_code = rsp.getcode()
 
-    if rsp_response_code == HTTPStatus.OK
+    if rsp_response_code is HTTPStatus.OK
         json_data = json.load(rsp)
     else
         data = {}
